90f6f5f64cb5cede1d6db12996cab68f9c750a16
nsbazhenov <nikel-90@mail.ru>
2022-10-16 19:23:03 +0300

    final version

--- src/main/java/com/github/nsbazhenov/challenge/controller/SuggestionsController.java
+++ src/main/java/com/github/nsbazhenov/challenge/controller/SuggestionsController.java
@@ -2,8 +2,6 @@ package com.github.nsbazhenov.challenge.controller;
 
 import com.github.nsbazhenov.challenge.model.Suggestions;
 import com.github.nsbazhenov.challenge.service.SuggestionsService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,8 +21,6 @@ import javax.validation.constraints.Min;
 @Validated
 public class SuggestionsController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SuggestionsService.class);
-
     private final SuggestionsService suggestionsService;
 
     public SuggestionsController(SuggestionsService suggestionsService) {
@@ -35,11 +31,6 @@ public class SuggestionsController {
     public ResponseEntity<Suggestions> getSuggestions(@RequestParam(value = "q") String searchQuery,
                                                      @RequestParam(value = "latitude", required = false) @Min(-90L) @Max(90L) double latitude,
                                                      @RequestParam(value = "longitude", required =  false) @Min(-180L) @Max(180L) double longitude) {
-        LOGGER.info("Received request to search for suggestions. SearchQuery - {}, latitude - {}, longitude - {}",
-                searchQuery, latitude, longitude);
-        ResponseEntity<Suggestions> response =
-                ResponseEntity.ok(suggestionsService.findSuggestions(searchQuery, latitude, longitude));
-        LOGGER.info("Response for search suggestions: {}", response);
-        return response;
+        return ResponseEntity.ok(suggestionsService.findSuggestions(searchQuery, latitude, longitude));
     }
 }
